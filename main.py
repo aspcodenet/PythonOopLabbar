8e3c05f1dc8711e640ea44173f436be3e9bb71dd
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-10-03 13:58:26 +0200

    a

--- main.py
+++ main.py
@@ -20,7 +20,20 @@ print(f"Arean är {width*height}")
 rect = Rectangle(width,height)
 print(f"Arean är {rect.CalculateArea()}")
 
+mario = Point(100,20)
+# När Mario för i spelet - sätt point to 0,0 (reset)
+mario.Reset()
+# om VK_RIGHT + VK_DOWN
+mario.Move(xDelta = 1,yDelta= 1) 
+
+luigi = Point(50,50)
+# om VK_RIGHT + VK_DOWN
+luigi.Move(xDelta = 2,yDelta= 1) 
+
+
+
 start = Point(1,1)
+
 end = Point(10,10)
 
 print(f"Vi startar labyinten på {start.GetX()}, {start.GetY()}")
